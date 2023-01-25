5806e5b00039b69f95c7802d028b6c6b4a3ec666
Akinseye Abayomi <abayomi.akinseye@gmail.com>
2023-01-25 10:38:45 +0100

    Initial Commit

---
+++ HandoutLMS/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  HandoutLMS
+//
+//  Created by Yomi on 1/25/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ HandoutLMS/HandoutLMSApp.swift
@@ -0,0 +1,17 @@
+//
+//  HandoutLMSApp.swift
+//  HandoutLMS
+//
+//  Created by Yomi on 1/25/23.
+//
+
+import SwiftUI
+
+@main
+struct HandoutLMSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
